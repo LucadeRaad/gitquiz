353d33abc8ab05fcea221773d471fe01679accbd
LucadeRaad <59776518+LucadeRaad@users.noreply.github.com>
2020-02-26 21:06:27 -0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
 		// sort
         for (int i=0; i<data.length; i++) {
-            for (int j=0; j<data.length-i-1; j++) {
+            for (int j=0; j>data.length-i-1; j++) {
                 if (data[j]>data[j+1]) {
                     swap(data, j);
                 }
